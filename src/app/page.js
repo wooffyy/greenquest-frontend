afb366fb1b99855e0c718969d3dae7acb4a67676
wafi <wahfiuddin3173@gmail.com>
2025-07-01 21:06:20 +0700

    Main Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,9 @@
-import Navbar from "../components/Navbar";
-import HeroSection from "../components/HeroSection";
-import Highlights from "../components/Highlights";
-import Statistics from "../components/Statistics";
-import ContactUs from "../components/ContactUs";
+import Main from "../components/Main";
 
 export default function LandingPage() {
   return (
-    <main className="bg-black text-white font-sans scroll-smooth">
-      <Navbar />
-      <section id="about"><HeroSection /></section>
-      <section id="highlights"><Highlights /></section>
-      <section id="statistics"><Statistics /></section>
-      <section id="contact"><ContactUs /></section>
-    </main>
+    <div>
+      <Main />
+    </div>
   );
 }
