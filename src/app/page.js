7b6efd350b65fdbc0a5fb9552c424e3bbc46893c
wafi <wahfiuddin3173@gmail.com>
2025-07-01 21:06:20 +0700

    Main Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Main from "../components/Main";
 
 export default function Home() {
   return (
-    <div>Test</div>
+    <div>
+      <Main />
+    </div>
   );
 }
---
+++ src/components/NavbarMain.js
@@ -0,0 +1,7 @@
+export default function NavbarMain(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
